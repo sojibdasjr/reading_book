d049831729e7f55d8034c8bc7aa8aa11bcf46e84
sojibdasjr <contact.sojibdas@gmail.com>
2024-02-18 21:49:27 +0600

    Project Done .. using: React Vite/React Icon/ Tailwilnd/ fake data

--- src/components/BooksMarks/BooksMarks.jsx
+++ src/components/BooksMarks/BooksMarks.jsx
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 const BooksMarks = ({ booksMarks, readingTime }) => {
   return (
     <div className="md:w-1/3 border ">
-      <h1 className="bg-yellow-50 m-4 text-center p-4 border border-yellow-500 text-2xl">
+      <h1 className="bg-yellow-50 m-2 text-center p-4 border border-yellow-500 text-2xl">
         Spent time on read :{readingTime} min
       </h1>
-      <div className=" bg-sky-200 pb-2">
+      <div className=" bg-sky-200 pb-2 mx-2">
         <h1 className="ms-4 text-2xl pt-2 underline">
           Bookmarked Blogs: {booksMarks.length}
         </h1>
